43e05b7e761e0fd034b6bdb8c9328db4209700b2
FernandoCelmer <email@fernandocelmer.com>
2025-02-23 20:54:48 -0300

    📌 ISSUE-#19: Improved API interface for decorators

--- dotflow/core/actions.py
+++ dotflow/core/actions.py
@@ -1,5 +1,8 @@
 """Action"""
 
+from typing import Any
+from warnings import warn
+
 from dotflow.core.context import Context
 
 
@@ -23,6 +26,7 @@ def action(func):
 
 
 def retry(max_retry):
+    warn(f"The 'retry' decorator is deprecated", DeprecationWarning, stacklevel=2)
     def inside(func):
         def wrapper(*args, **kwargs):
             attempt = 0
@@ -39,3 +43,47 @@ def retry(max_retry):
 
         return wrapper
     return inside
+
+
+class Action(object):
+
+    def __init__(self, func = None, retry: int = 1):
+        self._func = func
+        self._retry = retry
+
+    def __call__(self, *args, **kwargs):
+        if self._func:
+            if 'previous_context' in self._func.__code__.co_varnames:
+                previous_context = kwargs.get("previous_context") or Context()
+                return self.output(content=self.retry(*args, previous_context=previous_context))
+            else:
+                return self.output(content=self.retry(*args))
+
+        def wrapper(*_args, **_kwargs):
+            self._func = args[0]
+            if 'previous_context' in args[0].__code__.co_varnames:
+                previous_context = _kwargs.get("previous_context") or Context()
+                return self.output(content=self.retry(*_args, previous_context=previous_context))
+            else:
+                return self.output(content=self.retry(*_args))
+        return wrapper
+
+    def retry(self, *args, **kwargs):
+            attempt = 0
+            error_output = Exception()
+
+            while self._retry > attempt:
+                try:
+                    return self._func(*args, **kwargs)
+                except Exception as error:
+                    error_output = error
+                    attempt += 1
+
+            raise error_output
+
+    def output(self, content: Any):
+        if content:
+            if isinstance(content, Context):
+                return content
+            return Context(storage=content)
+        return Context()
--- tests/core/test_actions.py
+++ tests/core/test_actions.py
@@ -4,7 +4,7 @@ import logging
 from pytest import fixture
 
 from dotflow.core.context import Context
-from dotflow.core.actions import action
+from dotflow.core.actions import Action, action
 
 
 def dummy_step():
@@ -15,7 +15,7 @@ def dummy_step_previous_context(previous_context):
     logging.debug(previous_context.storage)
 
 
-class TestActions(unittest.TestCase):
+class TestMethodActions(unittest.TestCase):
 
     @fixture(autouse=True)
     def inject_fixtures(self, caplog):
@@ -29,6 +29,26 @@ class TestActions(unittest.TestCase):
 
     def test_action_with_previous_context(self):
         inside = action(dummy_step_previous_context)
+
+        with self._caplog.at_level(logging.DEBUG):
+            inside()
+            assert self._caplog.records[0].message == 'None'
+
+
+class TestClassActions(unittest.TestCase):
+
+    @fixture(autouse=True)
+    def inject_fixtures(self, caplog):
+        self._caplog = caplog
+
+    def test_instantiating_action_class(self):
+        inside = Action(dummy_step)
+        decorated_function = inside()
+
+        self.assertIsInstance(decorated_function, Context)
+
+    def test_action_class_with_previous_context(self):
+        inside = Action(dummy_step_previous_context)
 
         with self._caplog.at_level(logging.DEBUG):
             inside()
