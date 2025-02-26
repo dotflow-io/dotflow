920fc2db7eb075e3678c4fb7ceea4e68bc22bb67
FernandoCelmer <email@fernandocelmer.com>
2025-02-25 22:37:14 -0300

    📌 ISSUE-#23: Code improvement

--- dotflow/core/decorators/retry.py
+++ dotflow/core/decorators/retry.py
@@ -3,23 +3,23 @@
 from warnings import warn
 
 
-def retry(max_retry):
+def retry(max_retry: int):
     warn("The 'retry' decorator is deprecated", DeprecationWarning, stacklevel=2)
 
     def inside(func):
 
         def wrapper(*args, **kwargs):
             attempt = 0
-            error_output = Exception()
+            exception = Exception()
 
             while max_retry > attempt:
                 try:
                     return func(*args, **kwargs)
                 except Exception as error:
-                    error_output = error
+                    exception = error
                     attempt += 1
 
-            raise error_output
+            raise exception
 
         return wrapper
     return inside
