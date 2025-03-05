202067a9c3bd734dd48fe3e53d7210365de18712
FernandoCelmer <email@fernandocelmer.com>
2025-03-05 00:00:46 -0300

    ⚙️ FEATURE: Improvement of code examples

--- examples/step_class_result_task.py
+++ examples/step_class_result_task.py
@@ -6,9 +6,6 @@ from dotflow import DotFlow, action
 @action(retry=5)
 class Step:
 
-    def __init__(self):
-        """It is extremely important to have an '__init__' function!"""
-
     def auxiliary_function(self):
         """This function will not be executed, because
         it does not have an 'action' decorator.
