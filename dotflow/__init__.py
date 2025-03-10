9f292601c5942ee121f986881b88dca5cfec4a84
FernandoCelmer <email@fernandocelmer.com>
2025-03-10 15:38:35 -0300

    ⚙️ FEATURE: Removed unused decorators

--- dotflow/__init__.py
+++ dotflow/__init__.py
@@ -8,12 +8,10 @@ from .core.action import Action as action
 from .core.context import Context
 from .core.dotflow import DotFlow
 from .core.task import Task
-from .core.decorators import retry  # deprecated
 
 
 __all__ = [
     "action",
-    "retry",
     "DotFlow",
     "Context",
     "Config",
