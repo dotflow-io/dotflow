44c7d1d6a15a546b4ef56f7c822077fbccf2483b
FernandoCelmer <email@fernandocelmer.com>
2025-05-03 18:53:52 -0300

    📌 ISSUE-#56: Updated

--- dotflow/core/execution.py
+++ dotflow/core/execution.py
@@ -47,14 +47,14 @@ class Execution:
         task: Task,
         workflow_id: UUID,
         previous_context: Context = None,
-        _internal_callback: Callable = basic_callback,
+        _flow_callback: Callable = basic_callback,
     ) -> None:
         self.task = task
         self.task.status = TaskStatus.IN_PROGRESS
         self.task.previous_context = previous_context
         self.task.workflow_id = workflow_id
 
-        self._excution(_internal_callback)
+        self._excution(_flow_callback)
 
     def _is_action(self, class_instance: Callable, func: Callable):
         try:
@@ -136,7 +136,7 @@ class Execution:
         return new_context
 
     @time
-    def _excution(self, _internal_callback):
+    def _excution(self, _flow_callback):
         try:
             current_context = self.task.step(
                 initial_context=self.task.initial_context,
@@ -162,6 +162,6 @@ class Execution:
 
         finally:
             self.task.callback(task=self.task)
-            _internal_callback(task=self.task)
+            _flow_callback(task=self.task)
 
         return self.task
