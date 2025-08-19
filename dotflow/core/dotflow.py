3c6c7f0677cf1fb1943ec58c7376b430a1ab1df9
FernandoCelmer <email@fernandocelmer.com>
2025-08-18 23:29:15 -0300

    ⚙️ FEATURE: Dotflow Class Improvement

--- dotflow/core/dotflow.py
+++ dotflow/core/dotflow.py
@@ -1,15 +1,27 @@
-"""DotFlow"""
+"""DotFlow module"""
 
-from uuid import uuid4
+from uuid import uuid4, UUID
 from functools import partial
-from typing import Optional
+
+from typing import Callable, Optional
 
 from dotflow.core.config import Config
 from dotflow.core.workflow import Manager
 from dotflow.core.task import TaskBuilder
+from dotflow.utils.tools import start_and_validate_instance
 
 
-class DotFlow:
+class DotflowInstance:
+
+    def __init__(self, *_args, **_kwargs):
+        self._config: Config = None
+        self._workflow_id: UUID = None
+        self._task: TaskBuilder = None
+        self._add: Callable = None
+        self._start: Manager = None
+
+
+class DotFlow(DotflowInstance):
     """
     Import:
         You can import the **Dotflow** class directly from dotflow:
@@ -27,34 +39,83 @@ class DotFlow:
             workflow = DotFlow(config=config)
 
     Args:
-        config (Optional[Config]): Configuration class.
+        config (Optional[Config]): Config class.
 
     Attributes:
         workflow_id (UUID):
 
-        task (List[Task]):
+        task (TaskBuilder):
+
+        add (Callable):
 
         start (Manager):
     """
 
-    def __init__(
-            self,
-            config: Optional[Config] = None
-    ) -> None:
-        self.workflow_id = uuid4()
-        config = config if config else Config()
+    def __init__(self, config: Optional[Config] = Config) -> None:
+        super().__init__()
+        self.config: Config = config
+        self.workflow_id: UUID = None
+        self.task: TaskBuilder = None
+        self.add: Callable = None
+        self.start: Manager = None
 
-        self.task = TaskBuilder(
-            config=config,
-            workflow_id=self.workflow_id
-        )
-        self.add = self.task.add
+    @property
+    def config(self):
+        return self._config
 
-        self.start = partial(
-            Manager,
-            group=self.task.group,
-            workflow_id=self.workflow_id
-        )
+    @config.setter
+    def config(self, value):
+        if not value:
+            value = Config()
+
+        self._config = start_and_validate_instance(value, Config)
+
+    @property
+    def workflow_id(self):
+        return self._workflow_id
+
+    @workflow_id.setter
+    def workflow_id(self, value):
+        if not value:
+            value = uuid4()
+        self._workflow_id = value
+
+    @property
+    def task(self):
+        return self._task
+
+    @task.setter
+    def task(self, value):
+        if not value:
+            value = TaskBuilder(
+                config=self.config,
+                workflow_id=self.workflow_id
+            )
+        self._task = value
+
+    @property
+    def add(self):
+        return self._add
+
+    @add.setter
+    def add(self, value):
+        if not value:
+            value = self.task.add
+        self._add = value
+
+    @property
+    def start(self):
+        return self._start
+
+    @start.setter
+    def start(self, value):
+        if not value:
+            value = partial(
+                Manager,
+                group=self.task.group,
+                workflow_id=self.workflow_id
+            )
+        self._start = value
 
     def result_task(self):
         """
