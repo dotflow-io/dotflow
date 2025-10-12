f73e53ec475fc03bd243a130a19b2990382f623a
FernandoCelmer <email@fernandocelmer.com>
2025-10-12 04:08:17 -0300

    📦 PyPI: 1.0.0-dev-2

--- dotflow/__init__.py
+++ dotflow/__init__.py
@@ -1,6 +1,6 @@
 """Dotflow __init__ module."""
 
-__version__ = "1.0.0-dev-1"
+__version__ = "1.0.0-dev-2"
 __description__ = "🎲 Dotflow turns an idea into flow!"
 
 from .core.action import Action as action
