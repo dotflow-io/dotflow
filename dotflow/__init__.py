c59559003b22ef3cd961b77b46fcf63d77e6d67f
GitHub Action <action@github.com>
2026-04-15 04:47:34 +0000

    📦 PyPI: Update version to 1.0.0.dev4

--- dotflow/__init__.py
+++ dotflow/__init__.py
@@ -1,6 +1,6 @@
 """Dotflow __init__ module."""
 
-__version__ = "1.0.0.dev3"
+__version__ = "1.0.0.dev4"
 __description__ = "🎲 Dotflow turns an idea into flow!"
 
 from .core.action import Action as action
